af8367dc7fa5380e34c9c58787db7f5be0c0fa11
Tom Otero <tomotero1984@gmail.com>
2021-06-17 00:14:46 -0700

    adding database functions

--- src/node/ArduinoLCDSensorMonitorNODE/data_handler.js
+++ src/node/ArduinoLCDSensorMonitorNODE/data_handler.js
@@ -1,10 +1,30 @@
-require("node-gyp/package.json"); // node-gyp is a peer dependency. 
-var sqlite3 = require("sqlite3")
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(':memory:', (err) => {
+    if (err){
+        return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+});
+
 
 function DataHandler() {
     this.values = [];
     this.checkValues = () =>{
         console.log(this.values);
+    }
+
+    this.database_init = () => {
+        const sqlite3 = require('sqlite3').verbose();
+        this.db = new sqlite3.Database(':memory:', (err) => {
+            if (err) {
+                return console.error(err.message);
+            }
+            console.log('Connected to the in-memory SQlite database.');
+        });
+    }
+
+    this.database_close = () => {
+        this.db.close
     }
 }
 
