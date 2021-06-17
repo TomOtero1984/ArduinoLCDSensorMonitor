97fd669c3bbce0417386efa77cc2cac5a1c65c66
Tom Otero <tomotero1984@gmail.com>
2021-06-16 23:48:30 -0700

    adding sqlite3

--- src/node/ArduinoLCDSensorMonitorNODE/data_handler.js
+++ src/node/ArduinoLCDSensorMonitorNODE/data_handler.js
@@ -1,3 +1,6 @@
+require("node-gyp/package.json"); // node-gyp is a peer dependency. 
+var sqlite3 = require("sqlite3")
+
 function DataHandler() {
     this.values = [];
     this.checkValues = () =>{
